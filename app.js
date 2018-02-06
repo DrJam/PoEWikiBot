1948d2ee9907bc5a38a458385d90a2c719cc1655
daleroy1 <leonvd81@gmail.com>
2018-02-06 20:21:12 +0800

    Added some logging and try/catches

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const puppeteer = require('puppeteer');
 const wikiRegex = new RegExp("\\[\\[([^\\[\\]]*)\\]\\]", "gu");
 const urlRegex = new RegExp("\\w", "g");
 
+const log = require('simple-node-logger').createSimpleFileLogger('./logs/requests.log');
+const errorLog = require('simple-node-logger').createSimpleFileLogger('./logs/error.log');
+errorLog.setLevel('error');
+
 var config = require("./config.json");
 
 var client = new Discord.Client({
@@ -35,17 +39,21 @@ client.on("message", (message) => {
         //Disabled this check, as it causes it to not do a check if you post twice in a row.
         //return;
     }
+    try {
+        const server = message.guild.name;
 
-    let matches = wikiRegex.exec(message.cleanContent);
-    if (matches != null && matches.length > 0) {
-        for (let i = 1; i < matches.length; i++) {
-            handleItem(matches[i], message.channel);
+        let matches = wikiRegex.exec(message.cleanContent);
+        if (matches != null && matches.length > 0) {
+            for (let i = 1; i < matches.length; i++) {
+                handleItem(matches[i], message.channel, server);
+            }
         }
+    } catch (error) {
+        errorLog.error(new Date().toJSON(), ' ', error.message);
     }
 });
 
-async function handleItem(name, channel) {
-
+async function handleItem(name, channel, server) {
     console.log(name)
     let itemUrlPart = convertToUrlString(name);
     var url = config.wikiURL + itemUrlPart;
@@ -54,49 +62,62 @@ async function handleItem(name, channel) {
     await channel
         .send(initalMessage)
         .then(message => {
-            console.log(`Sent message: ${message.id}`);
+            //console.log(`Sent message: ${message.id} ${initialMessage}`);
             messageId = message.id;
-        });
-
-    await getImage(url)
-        .then((result) => {
-            if (result.success == false) {
-                channel
-                    .fetchMessage(messageId)
-                    .then(message => {
-                        message.edit("Could not get details from the Wiki for **" + name + "**");
-                    })
-                    .catch(console.log);
-            } else {
-                //need a way that lets us add an attachment message, currently I can only edit text to it
-                let output = '<' + url + '>';
-                //if no screenshot, just edit the original message
-                if (result.screenshot == false) {
-                    channel
-                        .fetchMessage(messageId)
-                        .then(message => {
-                            message.edit(output);
-                        })
-                        .catch(console.log);
-                } else {
-                    //otherwise delete the message and create a new one with the screenshot
-                    channel
-                        .fetchMessage(messageId)
-                        .then(message => {
-                            message.delete();
-                        })
-                        .catch(console.log);
-                    channel.send(output, { file: result.screenshot });
-                }
-                console.log('Found in the wiki and sent: ' + url);
-            }
         })
         .catch(error => {
-            console.log(error);
-        });
+            //console.log(error.message)
+            errorLog.error(new Date().toJSON(), ' ', error.message, ' - ', server, ' - ', name);
+        })
+
+    if (messageId != null) {
+        await getImage(url, server)
+            .then((result) => {
+                if (result.success == false) {
+                    channel
+                        .fetchMessage(messageId)
+                        .then(message => {
+                            message.edit("Could not get details from the Wiki for **" + name + "**");
+                        })
+                        .catch(error => {
+                            errorLog.error(new Date().toJSON(), ' Could not edit message ', messageId, ' - ', server, ' - ', name);
+                        })
+                } else {
+                    log.info(new Date().toJSON(), ' ', server, ' - ', name, ' - ', url);
+                    //need a way that lets us add an attachment message, currently I can only edit text to it
+                    let output = '<' + url + '>';
+                    //if no screenshot, just edit the original message
+                    if (result.screenshot == false) {
+                        channel
+                            .fetchMessage(messageId)
+                            .then(message => {
+                                message.edit(output);
+                            })
+                            .catch(error => {
+                                errorLog.error(new Date().toJSON(), ' Could not edit message ', messageId, ' - ', server, ' - ', name);
+                            })
+                    } else {
+                        //otherwise delete the message and create a new one with the screenshot
+                        channel
+                            .fetchMessage(messageId)
+                            .then(message => {
+                                message.delete();
+                            })
+                            .catch(error => {
+                                errorLog.error(new Date().toJSON(), ' Could not delete message ', messageId, ' - ', server, ' - ', name);
+                            })
+                        channel.send(output, { file: result.screenshot });
+                    }
+                    console.log('Found in the wiki and sent: ' + url);
+                }
+            })
+            .catch(error => {
+                errorLog.error(new Date().toJSON(), ' ', error.message, ' - ', server, ' - ', name);
+            })
+    }
 }
 
-async function getImage(url) {
+async function getImage(url, server) {
     console.time('getPage')
     const page = await browser.newPage();
     //Disabling Javascript adds 100% increased performance
@@ -113,8 +134,8 @@ async function getImage(url) {
         //played around with a few different waitUntils.  This one seemed the quickest.
         //If you don't disable Javascript on the PoE Wiki site, removing this parameter makes it hang
         await page.goto(url, { waitUntil: 'networkidle2' });
-    } catch (e) {
-        //console.log(e)
+    } catch (error) {
+        errorLog.error(new Date().toJSON(), ' ', error.message, ' - ', server, ' - ', url);
     }
 
     var invalidPage = await page.$(config.wikiInvalidPage);
